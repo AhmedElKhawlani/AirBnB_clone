9a5bd8162982bcae4c26189e36a243ac74712752
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-06-23 21:47:30 +0100

    Task 6

---
+++ console.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+Module that defines HBNBCommand class.
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    HBNBCommand : class that set up the command interpreter.
+    """
+    prompt = "(hbnb)"
+
+    def do_quit(self, line):
+        """
+        Exit the program.
+        """
+        return True
+
+    def do_EOF(self, line):
+        """
+        EOF to exit the program.
+        """
+        return True
+
+    def emptyline(self):
+        """
+        Do nothing on empty input line
+        """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
