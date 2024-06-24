76d09a63cf844a177b6fdca082d893d3a1e06cb5
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-06-24 18:38:05 +0100

    Task 10

--- console.py
+++ console.py
@@ -6,6 +6,11 @@ Module that defines HBNBCommand class.
 
 import cmd
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State 
 from models.user import User
 from models import storage
 
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,11 @@ This module defines the class FileStorage.
 
 import json
 from ..base_model import BaseModel
+from ..amenity import Amenity
+from ..city import City
+from ..place import Place
+from ..review import Review
+from ..state import State 
 from ..user import User
 
 
